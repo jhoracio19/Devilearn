503f809c9de3f6828ac883bb354def89b5fd7905
Jose Horacio <jhoracio19@hotmail.com>
2025-10-08 12:54:29 -0600

    urls y views de cursos

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def course_list(request):
+    pass
+
+def course_detail(request):
+    pass
+
+def course_lessons (request):
+    pass
