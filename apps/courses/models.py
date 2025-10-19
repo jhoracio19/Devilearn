a6e904e3fc8a51f2a5c53c4762dd56c4ec318345
Jose Horacio <jhoracio19@hotmail.com>
2025-10-19 01:46:13 -0600

    agregando modelo Review

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -4,5 +4,6 @@ from .models.course import Course, CourseCategory
 from .models.module import Module
 from .models.enrollment import Enrollment
 from .models.progress import Progress
+from .models.review import Review
 
 # Create your models here.
