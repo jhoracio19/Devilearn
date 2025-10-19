297daacaaba5a06255c5c9090d8e1099ecdc0b55
Jose Horacio <jhoracio19@hotmail.com>
2025-10-19 01:38:38 -0600

    agregando modelos enrollment y progress

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from .models.category import Category
 from .models.course import Course, CourseCategory
 from .models.module import Module
+from .models.enrollment import Enrollment
+from .models.progress import Progress
+
 # Create your models here.
